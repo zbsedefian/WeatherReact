1a1aafd8d668decce0180f278235f6a0e9c5c5e9
Zachary Sedefian <zbsedefian@gmail.com>
2018-09-11 18:46:44 -0400

    cors fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_KEY = '902f986f73917215ef5478937654385a'
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
-
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+const API_KEY = "902f986f73917215ef5478937654385a";
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`
-    const request = axios.get(url)
-        
-    return {
-        type: FETCH_WEATHER,
-        payload: request
-    }
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
 }
